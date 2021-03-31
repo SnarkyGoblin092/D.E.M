f6c08d9526ff83c10ea2f04c37ff9a1e3e707ea1
SnarkyGoblin092 <orsosemil092@gmail.com>
2021-03-31 16:49:17 +0200

    Upload project files

---
+++ Survival Game/Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    [SerializeField] Transform cameraPostion;
+
+    private void Update()
+    {
+        transform.position = cameraPostion.position;
+    }
+}
